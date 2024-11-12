dd15077b9e5d283cd8f8ca342ca31e09369629c2
Fadeluc-123 <mike@whiteoak.studio>
2024-11-11 19:17:42 -0500

    Added to "Ended" event by detecting if sound stops playing, regardless of .Ended firing

--- src/Modules/Events/Ended.lua
+++ src/Modules/Events/Ended.lua
@@ -1,13 +1,25 @@
 return function (sound: Sound, callback: () -> ())
-    local connection: RBXScriptConnection
-    connection = sound.Ended:Connect(function()
+    local connection1: RBXScriptConnection
+    local connection2: RBXScriptConnection
+
+    connection1 = sound.Ended:Connect(function()
         callback()
+    end)
+
+    connection2 = sound:GetPropertyChangedSignal("Playing"):Connect(function()
+        if not sound.Playing then
+            callback()
+        end
     end)
 
     -- Cleanup
     return function ()
-        if connection then
-            connection:Disconnect()
+        if connection1 then
+            connection1:Disconnect()
+        end
+
+        if connection2 then
+            connection2:Disconnect()
         end
     end
 end
